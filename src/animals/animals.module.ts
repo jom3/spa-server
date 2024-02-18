400f26c1c31e0f5867b6a6b88e7cc907a2c3cd89
joams mogro gomez <joamsmg@gmail.com>
2024-02-18 00:14:46 -0400

    attention entity added

--- src/animals/animals.module.ts
+++ src/animals/animals.module.ts
@@ -4,10 +4,11 @@ import { AnimalsController } from './animals.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Animal } from './entities/animal.entity';
 import { AnimalDetails } from './entities/animal.details.entity';
+import { Attention } from 'src/attention/attention/entities/attention.entity';
 
 @Module({
   imports:[
-    TypeOrmModule.forFeature([Animal, AnimalDetails])
+    TypeOrmModule.forFeature([Animal, AnimalDetails,Attention])
   ],
   controllers: [AnimalsController],
   providers: [AnimalsService],
