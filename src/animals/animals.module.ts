430142f6224a8ac0496529b86a1fc752339f9073
joams mogro gomez <joamsmg@gmail.com>
2024-02-22 23:00:08 -0400

    animal module updated

--- src/animals/animals.module.ts
+++ src/animals/animals.module.ts
@@ -5,10 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Animal } from './entities/animal.entity';
 import { AnimalDetails } from './entities/animal.details.entity';
 import { Attention } from 'src/attention/attention/entities/attention.entity';
+import { Adopt } from 'src/adopt/entities/adopt.entity';
 
 @Module({
   imports:[
-    TypeOrmModule.forFeature([Animal, AnimalDetails,Attention])
+    TypeOrmModule.forFeature([Animal, AnimalDetails,Attention,Adopt])
   ],
   controllers: [AnimalsController],
   providers: [AnimalsService],
